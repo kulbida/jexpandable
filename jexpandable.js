cf6a7adf40c11d7bd797e55d2bb3c5e9ea8dc9fd
Bogdan Kulbida <bkulbida@gmail.com>
2016-11-02 09:46:26 -0400

    Update jexpandable.js

--- jexpandable.js
+++ jexpandable.js
@@ -1,41 +1,43 @@
 jQuery.fn.extend({
 
   jexpandable: function() {
+
     this.each(function () {
 
-      var
-        _this = $(this),
-        text = _this.data('text'),
-        do_show = false;
-        CHARS_TO_SHOW = 120;
+      var _this       = $(this),
+        text          = _this.data('text'),
+        doShow        = false;
+        CHARS_TO_SHOW = _this.data('count') || 120;
 
-      if (!text)
-        throw "jexpandable: No text provided. Make sure text is provided in the `data-text` attribute."
+      if (typeof text === "undefined") {
+        throw "jexpandable: No text provided. Make sure text is provided in the `data-text` attribute.";
+      }
 
       function linkContainer(opts) {
-        return ' <span style="border-bottom: 1px  dashed;"> ' + opts.title + '</span>'
+        return ' <span style="border-bottom: 1px  dashed;"> ' + opts.title + '</span>';
       }
 
       function toggleVisibility() {
-        var
-          content;
 
-        if (do_show || text.length <= CHARS_TO_SHOW)
-          if (text.length > CHARS_TO_SHOW)
-            content = text + linkContainer({title: 'less'})
-          else
-            content = text
-        else {
-          if (text.indexOf("\n") == -1)
-            content = text.substr(0, CHARS_TO_SHOW)
-          else
-            content = text.split("\n")[0]
+        var content;
 
-          content += linkContainer({title: 'more'});
+        if (doShow || text.length <= CHARS_TO_SHOW) {
+          if (text.length > CHARS_TO_SHOW) {
+            content = text + linkContainer({title: 'see less'});
+          } else {
+            content = text;
+          }
+        } else {
+          if (text.indexOf("\n") == -1) {
+            content = text.substr(0, CHARS_TO_SHOW);
+          } else {
+            content = text.split("\n")[0];
+          }
+          content += "..." + linkContainer({title: 'see more'});
         }
 
         _this.html(content);
-        do_show = !do_show;
+        doShow = !doShow;
       };
       toggleVisibility();
 
